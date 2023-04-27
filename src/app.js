2d12f31889ae96e528ad743404f6f2a079953b2d
Peace Chinagwam <pchinagwam@gmail.com>
2023-04-27 22:05:34 +0100

    updates in html and css

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 function displayTemperature(response) {
+  console.log(response.data);
+
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = Math.round(response.data.main.temp);
-  temperatureElement.innerHTML = temperature;
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round
+    (response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
 }
-let apiKey = "892794fo690ac452af67bf2et14e703c";
-let apiUrl = "https://api.shecodes.io/weather/v1/forecast?query=Abuja&key=892794fo690ac452af67bf2et14e703c&units=metric";
 
 
+let apiKey = "0c4358b449bfbe877c6b8dc889c84c8e";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Abuja&appid=0c4358b449bfbe877c6b8dc889c84c8e&units=metric";
 axios.get(apiUrl).then(displayTemperature);

7a4c58083419605c2e924a62058ae8943b3db052
Peace Chinagwam <pchinagwam@gmail.com>
2023-04-28 10:52:22 +0100

    update on weather icon

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ function formatDate(timestamp) {
 
 
 function displayTemperature(response) {
- 
+
 
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -31,6 +31,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round
     (response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -39,8 +40,9 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round
     (response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 let apiKey = "0c4358b449bfbe877c6b8dc889c84c8e";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Abuja&appid=0c4358b449bfbe877c6b8dc889c84c8e&units=metric";
+let city = "Abuja"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
